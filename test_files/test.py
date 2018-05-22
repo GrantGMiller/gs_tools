3f94421e5ba7424c1df8e6f95ef01c89049ce692
gmiller <gmiller@extron.com>
2018-05-22 09:53:29 -0400

    plugin matrix dc: new methods for WriteStatus and SubscribeStatus

--- test_files/test.py
+++ test_files/test.py
@@ -1,6 +1,6 @@
 from gs_tools import MoveListItem
 
-l = ['a', 'b', 'c', 'X', 'd', 'e', 'f','g']
-MoveListItem(l, 'X', 2)
+l = ['a', 'b', 'c', 'X', 'd', 'e', 'f', 'g']
+l = MoveListItem(l, 'X', 2)
 
 print('l=', l)
